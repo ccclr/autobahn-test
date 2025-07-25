f3b789f67688ef655bcf3b4402f12f3ffaf5b617
root <root@instance-20250613-030604.asia-east2-a.c.just-creek-462112-j2.internal>
2025-07-25 12:18:12 +0000

    fixing cut condition

--- primary/src/core.rs
+++ primary/src/core.rs
@@ -1704,14 +1704,17 @@ impl Core {
         debug!("current proposals {:?}", current_proposals);
         debug!("prepare proposal tips {:?}", prepare_proposals);
 
-        if self.cut_condition_type == 1 {
-            debug!("validity_threshold");
-            new_tips.len() as u32 >= self.committee.validity_threshold()
-        }
-        else{
-            debug!("quorum_threshold");
-            new_tips.len() as u32 >= self.committee.quorum_threshold()
-        }
+        debug!("cut_condition = {}", self.cut_condition_type);
+        return new_tips.len() as u8 >= self.cut_condition_type
+
+        // if self.cut_condition_type == 1 {
+        //     debug!("cut_condition = 1");
+        //     new_tips.len() as u32 >= self.committee.validity_threshold()
+        // }
+        // else{
+        //     debug!("quorum_threshold");
+        //     new_tips.len() as u32 >= self.committee.quorum_threshold()
+        // }
     }
 
     #[async_recursion]
