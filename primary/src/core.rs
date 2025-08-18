83cf78772e51408dc922a1ef020bfb7a550a4f2c
root <root@instance-20250613-030604.asia-east2-a.c.just-creek-462112-j2.internal>
2025-08-18 16:51:15 +0000

    fixing vote delay

--- primary/src/core.rs
+++ primary/src/core.rs
@@ -2531,25 +2531,18 @@ impl Core {
             }
 
             AsyncEffectType::VoteDelay => {
-                match message.clone() {
-                    PrimaryMessage::ConsensusVote(m) => {
-                        debug!("Simulating Vote Delay: delay Vote");
-                        let egress_end_time = Instant::now() + Duration::from_millis(self.egress_penalty);
-                        debug!("current time is {:?}", Instant::now());
-                        debug!("egress penalty is {:?}", self.egress_penalty);
-                        debug!("msg egress end time is {:?}", egress_end_time);
-                        let actual_send_time = egress_end_time.min(self.current_egress_end);
-                        debug!("msg actual send time is {:?}", actual_send_time);
-                        self.egress_delay_queue.insert_at((message, height, author, consensus_handler), actual_send_time);
-                        return;
-                    }
-                    _ => { 
-                        debug!("Simulating Vote Delay: sending message normally");
-                        self.send_msg_normal(message, height, author, consensus_handler).await;
-                    }
+                if let PrimaryMessage::ConsensusVote(_) = &message {
+                    debug!("Simulating Vote Delay: delay Vote");
+                    let egress_end_time = Instant::now() + Duration::from_millis(self.egress_penalty);
+                    let actual_send_time = egress_end_time.min(self.current_egress_end);
+                    self.egress_delay_queue.insert_at((message, height, author, consensus_handler), actual_send_time);
+                } else {
+                    debug!("Simulating Vote Delay: sending message normally");
+                    self.send_msg_normal(message, height, author, consensus_handler).await;
                 }
-                return; 
+                return; // 确保函数在这里结束
             }
+            
             _ => {
                 panic!("not a valid effect")
             }
